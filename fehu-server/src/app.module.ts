9e0ed4beef66b71439eddce2e67743e48e43dbdf
Luis Leon <69682697+flleonx@users.noreply.github.com>
2023-03-05 12:08:13 -0500

    feat: database configuration added (#2)
    - feat: sequelize initial setup
    - feat: migrator and seeder added
    - feat: local database setup with docker compose
    
    Co-authored-by: edrpj <89166419+edrpj@users.noreply.github.com>

--- fehu-server/src/app.module.ts
+++ fehu-server/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// CONFIGURATION
+import configuration from './configuration';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          dialect: 'postgres',
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.user'),
+          password: configService.get<string>('database.password'),
+          database: configService.get<string>('database.database_name'),
+          autoLoadModels: true,
+          synchronize: false,
+          logging: false,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
